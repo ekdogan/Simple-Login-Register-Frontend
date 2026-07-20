736021539b359eb57b2399fb2855731032e700ac
ekdogan <eminkursatdogan@gmail.com>
2026-07-20 12:24:29 +0300

    back

--- ItemCategorizerApi/obj/Release/net10.0/ItemCategorizerApi.AssemblyInfo.cs
+++ ItemCategorizerApi/obj/Release/net10.0/ItemCategorizerApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItemCategorizerApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6fb340a134c4cbbac6a0d459db57983006de59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItemCategorizerApi")]
